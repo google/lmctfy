10bc74840c8048f053a2c0707c75c82b6bfa1f31
Nan Monnand Deng <monnand@gmail.com>
2014-01-24 23:51:07 -0500

    use new instead of malloc() in C++ code.

--- clmctfy/clmctfy.cc
+++ clmctfy/clmctfy.cc
@@ -31,16 +31,16 @@ int lmctfy_init_machine_raw(struct status *s, const void *spec, const int spec_s
 
 int lmctfy_init_machine(struct status *s, const Containers__Lmctfy__InitSpec *spec) {
   size_t sz = containers__lmctfy__init_spec__get_packed_size(spec);
-  void *buf = malloc(sz);
+  uint8_t *buf = new uint8_t[sz];
   int ret = 0;
-  containers__lmctfy__init_spec__pack(spec, (uint8_t *)buf);
+  containers__lmctfy__init_spec__pack(spec, buf);
   ret = lmctfy_init_machine_raw(s, buf, sz);
-  free(buf);
+  delete []buf;
   return ret;
 }
 
 int lmctfy_new_container_api(struct status *s, struct container_api **api) {
-  *api = (struct container_api *)malloc(sizeof(struct container_api));
+  *api = new container_api();
   (*api)->container_api_ = NULL;
   StatusOr<ContainerApi *> statusor_container_api = ContainerApi::New();
   RETURN_IF_ERROR_PTR(s, statusor_container_api, &((*api)->container_api_));
@@ -52,34 +52,34 @@ void lmctfy_release_container_api(struct container_api *api) {
     if (api->container_api_ != NULL) {
       // TODO(monnand): delete api->container_api_?
     }
-    free(api);
+    delete api;
   }
 }
 
 int lmctfy_container_api_get_container(struct status *s,
-                                       struct container **container,
+                                       struct container **c,
                                        const struct container_api *api,
                                        const char *container_name) {
-  *container = (struct container *)malloc(sizeof(struct container));
-  (*container)->container_ = NULL;
+  *c = new container();
+  (*c)->container_ = NULL;
 
   StatusOr<Container *> statusor = api->container_api_->Get(container_name);
-  RETURN_IF_ERROR_PTR(s, statusor, &((*container)->container_));
+  RETURN_IF_ERROR_PTR(s, statusor, &((*c)->container_));
   return STATUS_OK;
 }
 
 int lmctfy_container_api_create_container_raw(struct status *s,
-                                       struct container **container,
+                                       struct container **c,
                                        struct container_api *api,
                                        const char *container_name,
                                        const void *spec,
                                        const int spec_size) {
   ContainerSpec container_spec;
-  *container = (struct container *)malloc(sizeof(struct container));
-  (*container)->container_ = NULL;
+  *c = new container();
+  (*c)->container_ = NULL;
   container_spec.ParseFromArray(spec, spec_size);
 
   StatusOr<Container *> statusor = api->container_api_->Create(container_name, container_spec);
-  RETURN_IF_ERROR_PTR(s, statusor, &((*container)->container_));
+  RETURN_IF_ERROR_PTR(s, statusor, &((*c)->container_));
   return STATUS_OK;
 }
