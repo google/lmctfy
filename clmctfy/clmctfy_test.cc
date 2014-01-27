04b3c09071c2855a8d5ae4824d217a38e23831fd
Nan Monnand Deng <monnand@gmail.com>
2014-01-27 13:52:12 -0500

    working on unit tests.

---
+++ clmctfy/clmctfy_test.cc
@@ -0,0 +1,41 @@
+// Copyright 2013 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "lmctfy_mock.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "clmctfy.h"
+#include "util/task/statusor.h"
+
+using ::util::Status;
+using ::util::StatusOr;
+
+namespace containers {
+namespace lmctfy {
+
+StatusOr<ContainerApi *> ContainerApi::New() {
+  return new StrictMockContainerApi();
+}
+
+namespace {
+
+TEST(ClmctfyTest, NewContainerApi) {
+  EXPECT_EQ(1, 1);
+}
+
+}  // namespace
+}  // namespace lmctfy
+}  // namespace containers
+
